19fc66a4a61aa718d5e141507eec563abc282748
xybayue-tbk <xybayue@gmail.com>
2026-04-06 10:52:53 +0700

    deklarasi variable struct dan mengisi data

--- ArrayStruct02.cpp
+++ ArrayStruct02.cpp
@@ -9,5 +9,14 @@ struct mahasiswa
 };
 
 int main(){
-    
+        // deklarasi variable struct
+    mahasiswa mhs;
+
+    // mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "Isikan data usia : ";
+    cin >> mhs.umur;
 }
