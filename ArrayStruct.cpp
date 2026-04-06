31d732f7af74e658d9d40345d5055fd621865f40
xybayue-tbk <xybayue@gmail.com>
2026-04-06 10:43:11 +0700

    mengisi data melalui perulangan dan menampilkan data dengan perulangan

--- ArrayStruct.cpp
+++ ArrayStruct.cpp
@@ -26,5 +26,18 @@ int main(){
     cin >> datamu[3];
     cout << "Isikan data kelima : ";
     cin >> datamu[4];
-    
+
+    // mengisi data melalui perulangan
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Isikan data ke-" << (i + 1) << " : ";
+        cin >> datamu[i];
+    }
+
+    // menampilkan data dengan perulangan
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " : " << datamu[i] << endl;
+    }
+
 }
