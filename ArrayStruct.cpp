72209872cd09bd68c3cf3ed391b8ef1978027dab
xybayue-tbk <xybayue@gmail.com>
2026-04-06 10:37:01 +0700

    mengisi array

--- ArrayStruct.cpp
+++ ArrayStruct.cpp
@@ -10,4 +10,8 @@ int main(){
 
     // cara akses (membaca atau mengambil isi array)
     cout << "Isi data kedua : " << dataku[1] << endl;
+
+    // mengisi array
+    dataku[1] = 200;
+    cout << "Isi data kedua setelah dirubah : " << dataku[1] << endl;
 }
