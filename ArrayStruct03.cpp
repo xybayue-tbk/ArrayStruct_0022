e6070ef7f4555e47e7cdd11d34de0c98f0f5652d
xybayue-tbk <xybayue@gmail.com>
2026-04-06 11:00:09 +0700

    library dan struct

---
+++ ArrayStruct03.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+struct AlamatDetail
+{
+    struct desa;
+    struct kota;
+    
+};
+
+struct mahasiswa
+{
+    string nama;
+    AlamatDetail alamat;
+    int umur;
+};
+
